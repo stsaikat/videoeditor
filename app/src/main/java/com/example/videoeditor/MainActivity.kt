2e5d8e04025628b10a27db2ba9e5b412b9dff92e
Saikat <thestsaikat@gmail.com>
2021-02-28 15:41:06 +0600

    Added ffmpeg library

--- app/src/main/java/com/example/videoeditor/MainActivity.kt
+++ app/src/main/java/com/example/videoeditor/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.videoeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.madhavanmalolan.ffmpegandroidlibrary.Controller
 
 class MainActivity : AppCompatActivity() {
+    val controller = Controller.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
